709ceedd6af95faf8f8c41ccc0e6fa1896931aec
pedrolucas12 <pedrolucassantana@gmail.com>
2023-12-12 09:46:18 -0300

    Implement hashing with linear probing and heap operations

--- Prova3/QuestoesChaves.c
+++ Prova3/QuestoesChaves.c
@@ -64,3 +64,93 @@
 
 
 
+
+/*
+Resolva o problem da contagem para o fluxo de chaves  17 21 19 4 26 30 37  usando hashing com sondagem linear.  A tabela de dispersão deve ter tamanho 13 e a função de espalhamento deve ser o resto da divisão da chave por 13.  Faça uma figura do estado final da tabela de dispersão.
+*/
+    void hash(int *v, int n){
+        int i, j, h;
+        int hash[13];
+        for(i = 0; i < 13; i++){
+            hash[i] = -1;
+        }
+        for(i = 0; i < n; i++){
+            h = v[i]%13;
+            if(hash[h] == -1){
+                hash[h] = v[i];
+            }
+            else{
+                j = h;
+                while(hash[j] != -1){
+                    j++;
+                    if(j == 13){
+                        j = 0;
+                    }
+                }
+                hash[j] = v[i];
+            }
+        }
+    }
+
+
+    /* CERTO OU ERRADO
+    Um vetor em ordem decrescente é um heap decrescente?  Um vetor em ordem crescente é um heap crescente? 
+    */
+    CERTO E CERTO
+
+    /*
+    Insira os itens  E A S Y Q U E S T I O N, nessa ordem, em um heap decrescente inicialmente vazio. Mostre o heap que resulta.
+    */
+    void insert(int *v, int n){
+        int i, j, aux;
+        for(i = 1; i < n; i++){
+            j = i;
+            while(j > 0 && v[j] > v[(j-1)/2]){
+                aux = v[j];
+                v[j] = v[(j-1)/2];
+                v[(j-1)/2] = aux;
+                j = (j-1)/2;
+            }
+        }
+    }
+
+    /*
+    Desenhe todos os heaps que podem ser construídos com os itens  A B C D E .  Desenhe todos os heaps que podem ser construídos com os itens  A A A B B
+    */
+    A B C D E                 
+    E D C B A
+    E D C A B
+    E D A C B
+    E A D C B
+    A E D C B
+    A D E C B
+    A D C E B
+    A D C B E
+    A B D C E
+    A B C D E
+
+
+    /*
+    Encontre o mínimo. Acrescente um método min() à classe MaxPQ. Sua implementação deve usar tempo constante e uma quantidade constante de espaço adicional.
+    */
+    void min(int *v, int n){
+        int i, min;
+        min = v[0];
+        for(i = 1; i < n; i++){
+            if(v[i] < min){
+                min = v[i];
+            }
+        }
+        return min;
+    }
+
+    /*
+    Suponha que sua aplicação terá um número enorme de operações insert e apenas algumas poucas operação delMax. Qual implementação de PQ devo usar: heap, vetor ordenado, ou vetor não ordenado?
+    */
+    vetor ordenado
+
+    /*
+    Suponha que sua aplicação terá um número enorme de operações max e apenas algumas poucas operação insert e delMax. Qual implementação de PQ devo usar: heap, vetor ordenado, ou vetor não ordenado?
+    */
+    heap
+
