36cb5aa3ab32de5aadfeb5d54af66b743f6d09b2
pedrolucas12 <pedrolucassantana@gmail.com>
2023-12-13 20:41:58 -0300

    Fix bug in login functionality

--- PROVA4/aCODIGOS.C
+++ PROVA4/aCODIGOS.C
@@ -615,3 +615,359 @@ void buscaBinariaDecrescente ( int * vetor , int n , int x ) {
     }
     return -1 ;
 }
+
+//COUTING SORT
+/*
+Ordenar contando as chaves em um vetor auxiliar:
+    ▶ Cada índice é uma chave: limita quantidade de chaves
+    ▶ Determina a posição da uma chave, contando quantas chaves menores existem
+    ▶ Histograma dos números: distribuição das frequências
+    ▶ Cada chave é reposicionada na posição definitiva
+*/
+
+/*
+v [2 3 3 4 1 3 4 3 1 2 2 1 2 4 3 4 4 2 3 4]
+    ▶ Frequências:
+        ⋆ 0: 0 vezes
+        ⋆ 1: 3 vezes
+        ⋆ 2: 5 vezes
+        ⋆ 3: 6 vezes
+        ⋆ 4: 6 vezes
+    ▶ Ordenando:
+        ⋆ zero 0's, três 1's, cinco 2's, seis 3's, seis 4's
+v [1 1 1 2 2 2 2 2 3 3 3 3 3 3 4 4 4 4 4 4]
+*/
+
+/*
+Etapa 1: contar as frequências
+    ▶ Para o intervalo de chaves: 0 até R − 1
+    ▶ Utiliza-se: count[R+1]
+    ▶ Cada chave i em 0 até R − 1 = count[i]
+    ▶ Cada count[i] = frequência da chave i − 1 (imediatamente < i)
+*/
+
+// memset (count , 0 , sizeof ( int )*(R +1) )
+for (i =0; i <= R; i ++){
+    count [i] = 0;
+}
+for (i= l; i <= r; i ++){
+    count [v[i ]+1]++;
+}
+
+/*
+Etapa 2: Calculando as posições/índices através das frequências
+    ▶ Se count[i] contém a quantidade de chaves imediatamente menores que i,
+    ▶ Então, se, count[i] = count[i] + count[i-1] + ... + count[0],
+    ▶ count[i] vai conter a quantidade de todas as chaves menores que i,
+    ▶ count[i] vai conter a quantidade de posições que devem ser "puladas" para a
+    inserção das chaves menores que i,
+    ▶ Portanto, count[i] contém a distância de 0 até a chave i (posição de i)
+*/
+
+for (i =1; i <= R; i ++){
+    count[i] += count[i- 1];
+}
+
+/*
+Etapa 3: distribuindo as chaves
+    ▶ count[R+1]: tabela de índices
+    ▶ aux[r-l+1]: auxiliar para copiar as chaves na ordem,
+        ⋆ count[ v[i] ]: posição ordenada da chave v[i]
+        ⋆ aux[count[v[i]]] = v[i]: v[i] em sua posição ordenada em aux[ ]
+        ⋆ count[ v[i] ]++: próxima v[i] em sua posição ordenada em aux[ ]
+*/
+
+for ( i = l; i <= r; i ++) {
+    aux[count[v[i]]] = v[i];
+    count[v[i]]++;
+}
+
+
+
+/*
+Radix:
+    ▶ ordenar pela a raiz(radix) da representação dos dados
+    ▶ extraindo o i-ésimo digito da chave
+Obs.: caso a chave seja pequena não compensa a extração dos bits: use o
+counting sort
+*/
+
+
+// LSD
+
+/*
+A partir dígito menos significativo (least significant digit - LSD): direita para
+esquerda
+    ▶ Ordena estavelmente chaves de comprimento fixo
+        ⋆ Tamanho da palavra (word) que representa o dado
+        ⋆ int: 4 * 8 bits = 32 bits = 4 bytes
+        ⋆ strings de tamanho W
+Complexidade ≈ 7 ∗ W ∗ N + 3 ∗ W ∗ R acessos:
+    ▶ N chaves
+    ▶ chaves de tamanho W
+    ▶ cujo alfabeto são de tamanho R
+R, em geral, é muito menor que N, portanto a complexidade é proporcional a
+W ∗ N
+*/
+
+
+void radix_sortLSD (int *v , int l , int r) {
+int i , w;
+int aux [r-l +1] , count [R +1];
+
+    //w -> deslocamento de bytes
+    for (w =0; w < bytesword ; w ++) {
+        // for (i =0; i <=R; i++) count [i] = 0;
+        memset ( count , 0, sizeof (int) *( R +1) );
+
+        // frequencias
+        for (i= l; i <= r; i ++) {
+            count [ digit (v[ i], w ) +1 ]++;
+        }
+
+        // posições
+        for (i =1; i <= R; i ++) {
+            count [i] += count [i- 1];
+        }
+
+        // distruibuição
+        for (i= l; i <= r; i ++) {
+            aux [ count [ digit (v[i ], w ) ] ] = v[i ];
+            count [ digit (v[i ], w ) ]++;
+        }
+
+        // copiando : a partir de aux [0]
+        for (i= l; i <= r; i ++) {
+            v[i] = aux [i-l];
+        }
+    }
+}
+
+
+//MSD
+
+/*
+A partir dígito mais signigicativo (most significant digit - MSD): esquerda
+para direita
+    ▶ Ordenação de propósito geral: chaves com tamanhos variáveis
+        ⋆ Conjunto de várias palavras
+        ⋆ string: N * 8 bits = N * 1 byte, N variável
+    ▶ Usa-se primeiro o counting sort (key-indexed counting)
+    ▶ Depois, recursivamente, ordena-se os sub-vetores de cada caractere
+Complexidade ≈ 7 ∗ W ∗ N + 3 ∗ W ∗ R acessos:
+*/
+
+typedef char Item ;
+# define maxstring 101
+# define bitsbyte 8
+# define R (1 << bitsbyte ) // 00000001 < <8 = 2^8 = 256
+
+int charAt ( char s [] , int d) {
+    if (d < strlen ( s))
+        return s [d ]; //d-é simo caractere
+    else
+        return - 1; // count [-1 + 1] = count [0]
+    // count [0] = qtde . de palavras menores que d
+}
+
+// Strings : ordena para o d-é simo caractere
+void radixMSD ( char a [][ maxstring ] , int l , int r , int d) {
+    if(r <= l ) return ;
+
+    char aux [r-l +1][ maxstring ];
+
+    int count [R +1];
+    memset ( count , 0, sizeof (int) *( R +1) );
+    // for ( int i =0; i <=R; i++) count [i] = 0;
+    // frequencia dos d-é simos caracteres
+    for ( int i = l ; i <= r ; i ++)
+        count [ charAt (a[i ], d) + 1]++;
+
+    // tabela de í ndices : calculando as posi ções
+    for ( int i = 1; i <= R; i ++)
+        count [i] += count [i- 1];
+
+    // redistribui as chaves : ordena em aux
+    for ( int i = l ; i <= r ; i ++)
+        strcpy ( aux [ count [ charAt (a [i], d) ]++] , a [i ]) ;
+
+    // copia para o original
+    for ( int i = l ; i <= r ; i ++)
+        strcpy (a [i], aux [i - l ]) ;
+
+    // ordenar por subconjunto : count [0] = já ordenadas
+    for ( int i = 1; i <= R ; i ++) {
+        // count [i-1] posi ção da primeira chave com o caractere i
+        // count [i]-1 posi ção da ú ltima chave com o caractere i
+        radixMSD (a , l + count [i- 1] , l + count [ i]-1, d +1) ;
+    }
+}
+// Inteiros : ordena o w-é simo byte
+void radix_sortMSD (int *v , int l , int r , int w) {
+    if(r <= l || w < 0) return ;
+    int i , aux [r-l +1] , count [ R +1];
+    memset ( count , 0, sizeof (int) *( R +1) );
+
+    for (i= l; i <= r; i ++)
+        count [ digit (v[i], w) +1]++;
+
+    for (i =1; i <= R; i ++)
+        count [ i] += count [i- 1];
+
+    for (i= l; i <= r; i ++)
+        aux [ count [ digit (v [i], w) ]++] = v[i ];
+
+    for (i= l; i <= r; i ++) 
+        v[i] = aux [i-l ];
+
+    // ordenando os que come çam por zero
+    radix_sortMSD (v , l , l + count [0] -1, w- 1) ;
+    for ( i = 1; i <= R; i ++)
+        radix_sortMSD (v , l + count [i- 1] , l + count [i ]-1, w- 1) ;
+}
+
+// EXERCICIO DO TORNEIO
+p_no torneio(int *v, int l, int r) {
+	p_no esq, dir;
+	int valor, m = (l + r) / 2;
+	if (l == r)
+		return criar_arvore(v[l], NULL, NULL);
+	esq = torneio(v, l, m);
+	dir = torneio(v, m+1, r);
+	valor = esq->dado > dir->dado ? esq->dado : dir->dado;
+	return criar_arvore(valor, esq, dir);
+}
+
+//Percursos em profundidade
+//PRE ORDEM
+void pre_ordem(p_no raiz) {
+	if (raiz != NULL) {
+		printf("%d ", raiz->dado); /* visita raiz */
+		pre_ordem(raiz->esq);
+		pre_ordem(raiz->dir);
+	}
+}
+
+void pre_ordem(p_no raiz) {
+	p_pilha p; /* pilha de p_no */
+	p = criar_pilha();
+	empilhar(p, raiz);
+	while(!pilha_vazia(p)) {
+		raiz = desempilhar(p);
+		if (raiz != NULL) {
+			empilhar(p, raiz->dir);
+			empilhar(p, raiz->esq);
+			printf("%d ", raiz->dado); /* visita raiz */
+		}
+	}
+	destruir_pilha(p);
+}
+
+//POS ORDEM
+void pos_ordem(p_no raiz) {
+	if (raiz != NULL) {
+		pos_ordem(raiz->esq);
+		pos_ordem(raiz->dir);
+		printf("%d ", raiz->dado); /* visita raiz */
+	}
+}
+
+//INORDEM
+void inordem(p_no raiz) {
+	if (raiz != NULL) {
+		inordem(raiz->esq);
+		printf("%d ", raiz->dado); /* visita raiz */
+		inordem(raiz->dir);
+	}
+}
+
+// PERCURSO EM LARGURA
+void percurso_em_largura(p_no raiz) {
+	p_fila f;
+	f = criar_fila();
+	enfileirar(f, raiz);
+	while(!fila_vazia(f)) {
+		raiz = desenfileirar(f);
+		if (raiz != NULL) {
+			enfileirar(f, raiz->esq);
+			enfileirar(f, raiz->dir);
+			printf("%d ", raiz->dado); /* visita raiz */
+		}
+	}
+	destruir_fila(f);
+}
+
+/* EXERCÍCIOS */
+
+//////////////////////////////////////////
+// verifica se duas arvores são iguais //
+////////////////////////////////////////
+
+int sao_iguais(p_no raiz1, p_no raiz2) {
+    // Se ambas as árvores são nulas, elas são iguais.
+    if (raiz1 == NULL && raiz2 == NULL) {
+        return 1;
+    }
+
+    // Se uma árvore é nula e a outra não, elas não são iguais.
+    if (raiz1 == NULL || raiz2 == NULL) {
+        return 0;
+    }
+
+    // Verifique se os valores dos nós correspondentes são iguais.
+    if (raiz1->dado != raiz2->dado) {
+        return 0;
+    }
+
+    // Recursivamente verifique as subárvores esquerdas e direitas.
+    int esq_iguais = sao_iguais(raiz1->esq, raiz2->esq);
+    int dir_iguais = sao_iguais(raiz1->dir, raiz2->dir);
+
+    // As árvores são iguais apenas se ambas as subárvores esquerdas e direitas forem iguais.
+    return esq_iguais && dir_iguais;
+}
+
+////////////////////////////////////////////
+// APAGAR TODAS AS FOLHAS COM UM VALOR X //
+//////////////////////////////////////////
+
+p_no apagar_folhas_com_valor(p_no raiz, int valor) {
+    if (raiz == NULL) {
+        return NULL;
+    }
+
+    // Verifique se o nó atual é uma folha com o valor dado.
+    if (raiz->esq == NULL && raiz->dir == NULL && raiz->dado == valor) {
+        free(raiz); // Libera a memória do nó.
+        return NULL;
+    }
+
+    // Recursivamente processe as subárvores esquerda e direita.
+    raiz->esq = apagar_folhas_com_valor(raiz->esq, valor);
+    raiz->dir = apagar_folhas_com_valor(raiz->dir, valor);
+
+    return raiz;
+}
+
+///////////////////////////////////////////////////////////////////////////
+// Escreva uma função que calcula o número de folhas em uma árvore dada //
+/////////////////////////////////////////////////////////////////////////
+
+int numero_de_folhas(p_no raiz) {
+    if (raiz == NULL) {
+        return 0;
+    }
+    
+    // Se o nó atual é uma folha (sem filhos), retorne 1.
+    if (raiz->esq == NULL && raiz->dir == NULL) {
+        return 1;
+    }
+    
+    // Caso contrário, conte as folhas nas subárvores esquerda e direita e some-as.
+    int folhas_esq = numero_de_folhas(raiz->esq);
+    int folhas_dir = numero_de_folhas(raiz->dir);
+    
+    return folhas_esq + folhas_dir;
+}
+
+
